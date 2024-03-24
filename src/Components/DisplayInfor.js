0398dbe180af9eae0ef54102e3c79aeb55e797a5
Nino <ninoym94@gmail.com>
2024-03-24 16:58:25 +0700

    Function component - ko con this nua

--- src/Components/DisplayInfor.js
+++ src/Components/DisplayInfor.js
@@ -2,76 +2,64 @@ import React from "react";
 import "./DisplayInfor.scss";
 // import logo from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log(">>>call coonstructer:1");
-    super(props);
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+// class DisplayInfor extends React.Component {
+//   render() {
+//     // truyen` thuoc tinh tu cha xuong con
+//     // destructering : lay ra cac phan tu cua this.props
+//     const { listusers } = this.props;
+//     console.log(listusers);
+//     // cau dieu kien giup code ngan di
+//     return (
+//       <>
+//         {true && (
+//           <div className="display-infor-container">
+//             {/* <img src={logo} /> */}
+//             {listusers.map((user, index) => {
+//               return (
+//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                   <div>Name: {user.name}</div>
+//                   <div>Age: {user.age} </div>
+//                   <hr />
+//                   <button onClick={() => this.props.handleDeleteUser(user.id)}>
+//                     Delete
+//                   </button>
+//                 </div>
+//               );
 
-  componentDidMount() {
-    console.log(">>call me component did mount ");
-    setTimeout(() => {
-      document.title = "Suuu";
-    }, 3000);
-  }
+//               //convert string to number use ' + '
+//             })}
+//           </div>
+//         )}
+//       </>
+//     );
+//   }
+// }
 
-  componentDidUpdate(PrevProps, prevState, snapshot) {
-    console.log(">>call me component did update", this.props, PrevProps);
-    if (this.props.listusers !== PrevProps.listusers) {
-      if (this.props.listusers.length === 5) {
-        alert("vai loz");
-      }
-    }
-  }
+const DisplayInfor = (props) => {
+  const { listusers } = props;
 
-  handleShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser, //toggle
-    });
-  };
-  // template + logic js
-  render() {
-    console.log(">>call me render 3");
-    // truyen` thuoc tinh tu cha xuong con
-    // destructering : lay ra cac phan tu cua this.props
-    const { listusers } = this.props;
-    console.log(listusers);
-    // cau dieu kien giup code ngan di
-    return (
-      <>
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUser === true ? "Hide" : "Show me"}
-          </span>
+  return (
+    <>
+      {true && (
+        <div className="display-infor-container">
+          {listusers.map((user, index) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>Name: {user.name}</div>
+                <div>Age: {user.age} </div>
+                <hr />
+                <button onClick={() => props.handleDeleteUser(user.id)}>
+                  Delete
+                </button>
+              </div>
+            );
+
+            //convert string to number use ' + '
+          })}
         </div>
-        {this.state.isShowListUser && (
-          <div className="display-infor-container">
-            {/* <img src={logo} /> */}
-            {listusers.map((user, index) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>Name: {user.name}</div>
-                  <div>Age: {user.age} </div>
-                  <hr />
-                  <button onClick={() => this.props.handleDeleteUser(user.id)}>
-                    Delete
-                  </button>
-                </div>
-              );
+      )}
+    </>
+  );
+};
 
-              //convert string to number use ' + '
-            })}
-          </div>
-        )}
-      </>
-    );
-  }
-}
 export default DisplayInfor;
