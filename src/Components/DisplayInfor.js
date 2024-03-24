ddcd5d588ae9c6824c28b1ee3af76719aa1428fc
Nino <ninoym94@gmail.com>
2024-03-25 00:17:00 +0700

    useEffect giong voi component didmout//update

--- src/Components/DisplayInfor.js
+++ src/Components/DisplayInfor.js
@@ -1,41 +1,4 @@
-// import React, { useState } from "react";
-// import "./DisplayInfor.scss";
-// import logo from "./../logo.svg";
-
-// class DisplayInfor extends React.Component {
-//   render() {
-//     // truyen` thuoc tinh tu cha xuong con
-//     // destructering : lay ra cac phan tu cua this.props
-//     const { listusers } = this.props;
-//     console.log(listusers);
-//     // cau dieu kien giup code ngan di
-//     return (
-//       <>
-//         {true && (
-//           <div className="display-infor-container">
-//             {/* <img src={logo} /> */}
-//             {listusers.map((user, index) => {
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>Name: {user.name}</div>
-//                   <div>Age: {user.age} </div>
-//                   <hr />
-//                   <button onClick={() => this.props.handleDeleteUser(user.id)}>
-//                     Delete
-//                   </button>
-//                 </div>
-//               );
-
-//               //convert string to number use ' + '
-//             })}
-//           </div>
-//         )}
-//       </>
-//     );
-//   }
-// }
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 
 const DisplayInfor = (props) => {
@@ -46,6 +9,13 @@ const DisplayInfor = (props) => {
     setShowHideListUser(!isShowHideListUser);
   };
 
+  useEffect(() => {
+    if (listusers.length === 0) {
+      alert("oi mat riu`");
+    }
+    console.log(">call me neweffect");
+  }, [listusers]);
+  console.log(">call me render");
   return (
     <>
       <div className="display-infor-container">
