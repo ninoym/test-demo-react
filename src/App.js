1584d5c7a1cf5a7cd049a9424c0bef407b472f3f
Nino <ninoym94@gmail.com>
2024-03-28 01:37:18 +0700

    Link - Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.scss";
 import Header from "./Components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>test link</div>
+      <div>
+        <button>
+          <Link to="/users"> users </Link>
+        </button>
+        <button>
+          <Link to="/admin"> Admin </Link>
+        </button>
+      </div>
     </div>
   );
 };
