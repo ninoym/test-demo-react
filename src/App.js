6e5c5b4abf8b1a953bf0a26e49ada90537235d79
Nino <ninoym94@gmail.com>
2024-03-27 11:06:24 +0700

    New branch - Header bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import Header from "./Components/Header/Header";
 
 const App = () => {
-  return <div className="app-container">Hello worldddd</div>;
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
