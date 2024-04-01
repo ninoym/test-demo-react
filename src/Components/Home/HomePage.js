5de58f99d97104b855eb3d4fcf6a031a37a58852
Nino <ninoym94@gmail.com>
2024-04-01 10:38:15 +0700

    fix content

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -6,6 +6,16 @@ const HomePage = (props) => {
       <video autoPlay muted loop width="360px" height="480px">
         <source src={solar} type="video/mp4" />
       </video>
+      <div className="homepage-content">
+        <div className="Title-1">Make forms worth filling out</div>
+        <div className="Title-2">
+          Get more data—like signups, feedback, and anything else—with forms
+          designed to be refreshingly different.
+        </div>
+        <div>
+          <button>Get Started</button>
+        </div>
+      </div>
     </div>
   );
 };
