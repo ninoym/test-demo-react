7c3a25eec87fabc62480f839d99fcd105fd4b55c
Nino <ninoym94@gmail.com>
2024-04-01 10:32:13 +0700

    Upload video - design HomePage

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,5 +1,13 @@
+import solar from "../../assets/solar.mp4";
+
 const HomePage = (props) => {
-  return <div className="homepage-container">Home page Components</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop width="360px" height="480px">
+        <source src={solar} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
